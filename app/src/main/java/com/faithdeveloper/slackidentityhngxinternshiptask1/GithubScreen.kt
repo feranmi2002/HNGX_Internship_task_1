b5a47f809bf88e561ef54c88482dc693aa3f67d1
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2023-09-09 16:02:04 +0100

    Implemented Navigation between the two screens

--- app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/GithubScreen.kt
+++ app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/GithubScreen.kt
@@ -3,25 +3,61 @@ package com.faithdeveloper.slackidentityhngxinternshiptask1
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import kotlinx.coroutines.delay
 
 @Composable
 fun GithubScreen() {
+
+    var progress by remember {
+        mutableIntStateOf(0)
+    }
+
     val url = "https://www.github.com/feranmi2002"
+
     val context = LocalContext.current
+
     val webView = WebView(context)
+
     webView.layoutParams = ViewGroup.LayoutParams(
         ViewGroup.LayoutParams.MATCH_PARENT,
         ViewGroup.LayoutParams.MATCH_PARENT
     )
+
     webView.webViewClient = WebViewClient()
+
     webView.loadUrl(url)
-    AndroidView(factory = {
-        webView
-    }, update = {
-        it.loadUrl(url)
-    })
+
+    LaunchedEffect(key1 = progress) {
+        while (progress < 100) {
+            delay(1000)
+            progress = webView.progress
+        }
+    }
+
+    if (progress < 100) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator()
+        }
+    }else{
+        AndroidView(factory = {
+            webView
+        }, update = {
+            it.loadUrl(url)
+        })
+    }
+
 
 }
--- app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/ui/theme/MainScreen.kt
+++ app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/ui/theme/MainScreen.kt
@@ -2,6 +2,7 @@ package com.faithdeveloper.slackidentityhngxinternshiptask1.ui.theme
 
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
@@ -12,13 +13,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.faithdeveloper.slackidentityhngxinternshiptask1.R
 
-@Preview(showBackground = true)
 @Composable
 fun MainScreen(onGithubClick:() -> Unit) {
 
@@ -53,10 +54,12 @@ fun MainScreen(onGithubClick:() -> Unit) {
         ) {
             Image(
                 modifier = Modifier
-                    .size(300.dp)
-                    .clip(CircleShape),
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .border(2.dp, MaterialTheme.colorScheme.onBackground, CircleShape),
                 bitmap = BitmapFactory.decodeStream(context.resources.openRawResource(R.raw.profile_picture))
                     .asImageBitmap(),
+                contentScale = ContentScale.Crop,
                 contentDescription = "My profile picture"
             )
             Text(
