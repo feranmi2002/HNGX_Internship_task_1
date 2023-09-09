0ae59f0f2a92c1ab2597aa71df0e8c5a0519aa91
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2023-09-09 13:46:10 +0100

    Implemented Main Screen UI

--- app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/ui/theme/MainScreen.kt
+++ app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/ui/theme/MainScreen.kt
@@ -1,27 +1,79 @@
 package com.faithdeveloper.slackidentityhngxinternshiptask1.ui.theme
 
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.faithdeveloper.slackidentityhngxinternshiptask1.R
 
+@Preview(showBackground = true)
 @Composable
-fun MainScreen(){
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "My name")
-        Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "My profile picture")
+fun MainScreen() {
+
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Faith Oluwaoyehun", style = MaterialTheme.typography.displaySmall)
+            Text(
+                text = "Slack Name",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.surfaceVariant
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(300.dp)
+                    .clip(CircleShape),
+                bitmap = BitmapFactory.decodeStream(context.resources.openRawResource(R.raw.profile_picture))
+                    .asImageBitmap(),
+                contentDescription = "My profile picture"
+            )
+            Text(
+                text = "Slack Display Picture",
+                style = MaterialTheme.typography.bodySmall,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.surfaceVariant
+            )
+        }
+
         Button(onClick = {
 //            open github web page
         }) {
-            Text(modifier = Modifier.wrapContentSize(align = Alignment.Center), text = "OPEN GITHUB")
+            Text(
+                modifier = Modifier.wrapContentSize(align = Alignment.Center),
+                text = "OPEN GITHUB"
+            )
         }
-
     }
 }
