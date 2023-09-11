79156137bcc881764e6c1fae5638dd5f3af1bbb0
feranmi2002 <oluwaoyehunferanmi@gmail.com>
2023-09-11 11:47:06 +0100

    Implemented webview loading in GithubScreen.kt

--- app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/GithubScreen.kt
+++ app/src/main/java/com/faithdeveloper/slackidentityhngxinternshiptask1/GithubScreen.kt
@@ -7,23 +7,35 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import kotlinx.coroutines.delay
 
 @Composable
 fun GithubScreen() {
 
-    var progress by remember {
-        mutableIntStateOf(0)
+    var loadState: LOAD_STATE by rememberSaveable {
+        mutableStateOf(LOAD_STATE.LOADING)
     }
 
     val url = "https://www.github.com/feranmi2002"
 
     val context = LocalContext.current
+
+    class MyWebViewClient : WebViewClient() {
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            loadState = LOAD_STATE.LOADED
+        }
+    }
+
 
     val webView = WebView(context)
 
@@ -32,32 +44,34 @@ fun GithubScreen() {
         ViewGroup.LayoutParams.MATCH_PARENT
     )
 
-    webView.webViewClient = WebViewClient()
+    webView.webViewClient = MyWebViewClient()
 
-    webView.loadUrl(url)
-
-    LaunchedEffect(key1 = progress) {
-        while (progress < 100) {
-            delay(1000)
-            progress = webView.progress
-        }
+    if (loadState != LOAD_STATE.LOADED) {
+        webView.loadUrl(url)
     }
 
-    if (progress < 100) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            CircularProgressIndicator()
+    when (loadState) {
+        LOAD_STATE.LOADING -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator()
+            }
         }
-    }else{
-        AndroidView(factory = {
-            webView
-        }, update = {
-            it.loadUrl(url)
-        })
+
+        LOAD_STATE.LOADED -> {
+            AndroidView(factory = {
+                webView
+            }, update = {
+                it.loadUrl(url)
+            })
+        }
     }
+}
 
-
+enum class LOAD_STATE {
+    LOADING,
+    LOADED
 }
